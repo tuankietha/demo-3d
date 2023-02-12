503bb2e0abe299465282eb5b7e9580387aef528c
Nguyenthuan <prorockman113@gmail.com>
2023-02-13 03:15:58 +0700

    deploy on gh-pages

---
+++ dist/_nuxt/static/1676232899/ja/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdemo-3d\u002F_nuxt\u002Fstatic\u002F1676232899",layout:"default",error:a,serverRendered:true,routePath:"\u002Fja",config:{_app:{basePath:"\u002Fdemo-3d\u002F",assetsPath:"\u002Fdemo-3d\u002F_nuxt\u002F",cdnURL:a}}}}(null));
