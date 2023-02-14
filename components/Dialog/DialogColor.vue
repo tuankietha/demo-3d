b8fa42cf3ee5546544fff90eca9f3a3068cac34c
LeNhuHoang <106296783+tuankietha@users.noreply.github.com>
2023-02-14 16:14:31 +0700

    change

---
+++ components/Dialog/DialogColor.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-dialog
+		v-model="dialog"
+		:scrim="false"
+		width="auto"
+	>
+		<v-card flat width="200px">
+			askdjflkasd
+		</v-card>
+	</v-dialog>
+</template>
+
+<script>
+export default {
+    name: "DialogColor",
+    data: function(){
+        return{
+            dialog: false
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,13 @@
 		</v-sheet>
 		<div style="height: 100%; width: 100%; position: absolute" class="pointer-events-none">
 			<div class="d-flex flex-column justify-center pointer-events-none" style="height: 100%;">
-				<v-card height="50px" width="50px" color="red" class="ma-2 pointer-events-auto" @click="red" />
+				<v-card height="50px" width="50px" color="red" class="ma-2 pointer-events-auto" @click="red">
+					<!-- <v-color-picker
+					v-model="c2"
+					hide-inputs
+					></v-color-picker>
+					Choose color -->
+				</v-card>
 				<v-card height="50px" width="50px" color="green" class="ma-2 pointer-events-auto" @click="greeen" />
 				<v-card height="50px" width="50px" color="#f1c232" class="ma-2 pointer-events-auto" @click="yellow" />
 				<v-card height="50px" width="50px" color="white" class="ma-2 pointer-events-auto" @click="white" />
@@ -23,15 +29,21 @@ import { TransformControls } from "../node_modules/three/examples/jsm/controls/T
 import { RoomEnvironment } from "../node_modules/three/examples/jsm/environments/RoomEnvironment.js";
 export default {
 	name: "IndexPage",
+	data:()=>{
+		return{
+
+		}
+	},
 	methods: {
 		red () {
+			// this.$refs.dialogColor.dialog = true;
 			const mesh = this.$refs.player.scene.getObjectByName(this.$refs.player.mesh);
-
-			mesh.material.color.r = 244;
+			mesh.material.color.r = 1;
 			mesh.material.color.g = 1;
 			mesh.material.color.b = 1;
 			mesh.material.envMapIntensity = 0;
 			console.log(this.$refs.player.scene);
+			console.log(mesh.material);
 		},
 		greeen () {
 			const mesh = this.$refs.player.scene.getObjectByName(this.$refs.player.mesh);
@@ -83,6 +95,11 @@ export default {
 		// 	raycaster.setFromCamera(mousePosition, THREE.Camera);
 		// 	console.log(raycaster);
 		// });
+		// if(this.$refs.player.mesh != null){
+		// 	const mesh = this.$refs.player.scene.getObjectByName(this.$refs.player.mesh);
+
+		// 	// mesh.material.color = new THREE.Color( 0xff0000 )
+		// }
 	}
 };
 </script>
